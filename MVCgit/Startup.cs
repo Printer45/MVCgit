36d1d9db752bea2f57a2069b330c38dc608fba8c
boatp <boatp@DESKTOP-LSLEPLT>
2022-01-11 12:19:40 +0700

    Add project files.

---
+++ MVCgit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCgit.Startup))]
+namespace MVCgit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
